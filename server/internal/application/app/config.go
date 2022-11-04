1e28dbec031c3723de588ce332023e8aa89f9675
Alex Rundquist <alrund@gmail.com>
2022-11-04 15:36:25 +0300

    Fix config

--- server/internal/application/app/config.go
+++ server/internal/application/app/config.go
@@ -10,8 +10,8 @@ type Config struct {
 	RunAddress   string `env:"RUN_ADDRESS" env-default:"localhost:3000"`
 	DatabaseURI  string `env:"DATABASE_URI" env-default:"postgres://dev:dev@localhost:5432/dev?sslmode=disable"`
 	CipherPass   string `env:"CIPHER_PASSWORD" env-default:"J53RPX6"`
-	CertFile     string `env:"CERT_FILE" json:"cert_file"`
-	KeyFile      string `env:"KEY_FILE" json:"key_file"`
+	CertFile     string `env:"CERT_FILE" env-default:"local.crt"`
+	KeyFile      string `env:"KEY_FILE" env-default:"local.key"`
 }
 
 func NewConfig(loader port.ConfigLoader) (*Config, error) {
@@ -19,12 +19,22 @@ func NewConfig(loader port.ConfigLoader) (*Config, error) {
 
 	flags := NewFlags()
 	cfg.Debug = flags.Debug
-	cfg.RunAddress = flags.A
-	cfg.DatabaseURI = flags.D
 
 	if err := loader.Load(cfg); err != nil {
 		return nil, err
 	}
 
+	ReadFlags(flags, cfg)
+
 	return cfg, nil
+}
+
+func ReadFlags(f *Flags, cfg *Config) {
+	if f.A != NotAvailable {
+		cfg.RunAddress = f.A
+	}
+
+	if f.D != NotAvailable {
+		cfg.DatabaseURI = f.D
+	}
 }
