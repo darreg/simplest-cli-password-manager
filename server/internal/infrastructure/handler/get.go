816c70775633de76d8ee83db2c3e894d0b6aac94
Alex Rundquist <alrund@gmail.com>
2022-11-06 23:33:03 +0300

    Add grpc get method

---
+++ server/internal/infrastructure/handler/get.go
@@ -0,0 +1,34 @@
+package handler
+
+import (
+	"context"
+	"errors"
+	"github.com/alrund/yp-2-project/server/internal/application/usecase"
+	"github.com/alrund/yp-2-project/server/pkg/proto"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+// Get returns an Entry.
+func (c *Collection) Get(ctx context.Context, in *proto.GetRequest) (*proto.GetResponse, error) {
+	entry, err := usecase.GetEntry(ctx, in.EntryId, c.a.EntryRepository, c.a.UserRepository)
+	if err != nil {
+		switch {
+		case errors.Is(err, usecase.ErrEntryNotFound):
+			return nil, status.Error(codes.NotFound, codes.NotFound.String())
+		default:
+			return nil, status.Error(codes.Internal, codes.Internal.String())
+		}
+	}
+
+	return &proto.GetResponse{
+		EntryId:   entry.ID.String(),
+		TypeId:    entry.TypeID.String(),
+		Name:      entry.Name,
+		Metadata:  entry.Metadata,
+		Data:      entry.Data,
+		CreatedAt: timestamppb.New(*entry.CreatedAt),
+		UpdatedAt: timestamppb.New(*entry.UpdatedAt),
+	}, nil
+}
