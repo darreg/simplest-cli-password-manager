66673a8bc42e6d6e3edf3870870d636b786c5ea0
Alex Rundquist <alrund@gmail.com>
2022-11-09 21:55:09 +0300

    Add GetAllTypes usecase

---
+++ server/internal/application/usecase/getalltypes.go
@@ -0,0 +1,24 @@
+package usecase
+
+import (
+	"context"
+	"errors"
+
+	"github.com/alrund/yp-2-project/server/internal/domain/entity"
+	"github.com/alrund/yp-2-project/server/internal/domain/port"
+)
+
+func GetAllTypes(
+	ctx context.Context,
+	typeRepository port.TypeAllGetter,
+) ([]*entity.Type, error) {
+	types, err := typeRepository.GetAll(ctx)
+	if err != nil {
+		if errors.Is(err, ErrTypeNotFound) {
+			return nil, ErrTypeNotFound
+		}
+		return nil, ErrInternalServerError
+	}
+
+	return types, nil
+}
---
+++ server/internal/infrastructure/handler/getalltypes.go
@@ -0,0 +1,37 @@
+package handler
+
+import (
+	"context"
+	"errors"
+	"github.com/alrund/yp-2-project/server/internal/application/usecase"
+	"github.com/alrund/yp-2-project/server/pkg/proto"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+// GetAllTypes returns a slice of all types.
+func (c *Collection) GetAllTypes(
+	ctx context.Context,
+	in *proto.GetAllTypesRequest,
+) (*proto.GetAllTypesResponse, error) {
+	types, err := usecase.GetAllTypes(ctx, c.a.TypeRepository)
+	if err != nil {
+		switch {
+		case errors.Is(err, usecase.ErrTypeNotFound):
+			return nil, status.Error(codes.NotFound, codes.NotFound.String())
+		default:
+			return nil, status.Error(codes.Internal, codes.Internal.String())
+		}
+	}
+
+	responseTypes := make([]*proto.GetAllTypesResponse_Type, len(types))
+	for i, tp := range types {
+		responseTypes[i] = &proto.GetAllTypesResponse_Type{
+			TypeId:   tp.ID.String(),
+			Name:     tp.Name,
+			IsBinary: tp.IsBinary,
+		}
+	}
+
+	return &proto.GetAllTypesResponse{Types: responseTypes}, nil
+}
