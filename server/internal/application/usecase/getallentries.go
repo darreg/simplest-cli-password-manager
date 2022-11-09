d25ccdad52f136f08f41d9ef7aeabc07b49fe6fb
Alex Rundquist <alrund@gmail.com>
2022-11-09 21:54:50 +0300

    Add GetAllEntries usecase

---
+++ server/internal/application/usecase/getallentries.go
@@ -0,0 +1,36 @@
+package usecase
+
+import (
+	"context"
+	"errors"
+
+	"github.com/alrund/yp-2-project/server/internal/domain/entity"
+	"github.com/alrund/yp-2-project/server/internal/domain/port"
+)
+
+func GetAllEntries(
+	ctx context.Context,
+	entryRepository port.EntryAllByUserGetter,
+	userRepository port.UserGetter,
+) ([]*entity.Entry, error) {
+	contextSession := ctx.Value(port.SessionContextKey)
+	session, ok := contextSession.(*entity.Session)
+	if !ok {
+		return nil, ErrIncorrectSession
+	}
+
+	user, err := userRepository.Get(ctx, session.UserID)
+	if err != nil {
+		return nil, ErrUserNotFound
+	}
+
+	entries, err := entryRepository.GetAllByUser(ctx, user)
+	if err != nil {
+		if errors.Is(err, ErrEntryNotFound) {
+			return nil, ErrEntryNotFound
+		}
+		return nil, ErrInternalServerError
+	}
+
+	return entries, nil
+}
---
+++ server/internal/infrastructure/handler/getallentries.go
@@ -0,0 +1,40 @@
+package handler
+
+import (
+	"context"
+	"errors"
+	"github.com/alrund/yp-2-project/server/internal/application/usecase"
+	"github.com/alrund/yp-2-project/server/pkg/proto"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+// GetAllEntries returns a slice of all entries.
+func (c *Collection) GetAllEntries(
+	ctx context.Context,
+	in *proto.GetAllEntriesRequest,
+) (*proto.GetAllEntriesResponse, error) {
+	entries, err := usecase.GetAllEntries(ctx, c.a.EntryRepository, c.a.UserRepository)
+	if err != nil {
+		switch {
+		case errors.Is(err, usecase.ErrEntryNotFound):
+			return nil, status.Error(codes.NotFound, codes.NotFound.String())
+		default:
+			return nil, status.Error(codes.Internal, codes.Internal.String())
+		}
+	}
+
+	responseEntries := make([]*proto.GetAllEntriesResponse_Entry, len(entries))
+	for i, entry := range entries {
+		responseEntries[i] = &proto.GetAllEntriesResponse_Entry{
+			EntryId:   entry.ID.String(),
+			TypeId:    entry.TypeID.String(),
+			Name:      entry.Name,
+			CreatedAt: timestamppb.New(*entry.CreatedAt),
+			UpdatedAt: timestamppb.New(*entry.UpdatedAt),
+		}
+	}
+
+	return &proto.GetAllEntriesResponse{Entries: responseEntries}, nil
+}
