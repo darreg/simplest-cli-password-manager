86cdf2fc64bdc91488ab81f19ee04b455ae68691
Alex Rundquist <alrund@gmail.com>
2022-11-11 00:01:14 +0300

    Add request log

---
+++ server/internal/infrastructure/interceptor/requestlog.go
@@ -0,0 +1,40 @@
+package interceptor
+
+import (
+	"context"
+	"github.com/alrund/yp-2-project/server/internal/domain/entity"
+	"github.com/alrund/yp-2-project/server/internal/domain/port"
+	"google.golang.org/grpc"
+)
+
+// RequestLog logs all requests.
+func RequestLog(logger port.Logger) func(
+	ctx context.Context,
+	req interface{},
+	info *grpc.UnaryServerInfo,
+	unaryHandler grpc.UnaryHandler,
+) (interface{}, error) {
+	fn := func(
+		ctx context.Context,
+		req interface{},
+		info *grpc.UnaryServerInfo,
+		unaryHandler grpc.UnaryHandler,
+	) (interface{}, error) {
+
+		var userID string
+		contextSession := ctx.Value(port.SessionContextKey)
+		session, ok := contextSession.(*entity.Session)
+		if ok {
+			userID = session.UserID.String()
+		}
+
+		logger.Info("request",
+			"method", info.FullMethod,
+			"userID", userID,
+		)
+
+		return unaryHandler(ctx, req)
+	}
+
+	return fn
+}
